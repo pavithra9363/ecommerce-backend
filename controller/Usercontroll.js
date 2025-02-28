d643d3c6bf33d2152dd2e1e17464b0588cbd94c1
pavithra9363 <pavithrakarthik6383@gmail.com>
2025-02-28 12:04:26 +0530

    change bcrypt

--- controller/Usercontroll.js
+++ controller/Usercontroll.js
@@ -1,6 +1,6 @@
 import userModel from "../models/UserModel.js";
 import validator from "validator";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 // Function to create a JWT token
